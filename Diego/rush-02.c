6e9507beaafd8ef5c4216d5fbc78c9a462d89de2
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-03-13 20:06:46 -0400

    code base, missing fixes

--- Diego/rush-02.c
+++ Diego/rush-02.c
@@ -6,29 +6,39 @@
 /*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 21:31:10 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/13 14:57:12 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/13 19:49:16 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
+#include <fcntl.h>
 
-int	ft_strlen(char *str);
+// string.c
+char	*ft_get_file(int fildes);
 
-int	max_lenght;
-char *words;
+// main_logic.
+void	ft_main_logic(char *num, char *dict);
 
-int	main(int arg, char **strs)
+int	main(int argc, char **argv)
 {
-	int dict;
-	char *all_dict;
-	if (arg == 2)
+	int		fildes;
+	char	*k;
+	char	*name;
+	int		i;
+
+	i = 0;
+	if (argc >= 2 && argc <= 3)
 	{
-		dict = open("numbers.dict", O_RDONLY);
-	}
-	else
-	{
-		write(1, "error", 5);
+		if (argc == 3)
+		{
+			fildes = open(argv[2], O_RDONLY);
+		}
+		else
+		{
+			fildes = open("numbers.dict", O_RDONLY);
+		}
+		k = ft_get_file(fildes);
+		ft_main_logic(argv[1], k);
 	}
 	write(1, "\n", 1);
 }
--- Diego/string.c
+++ Diego/string.c
@@ -3,43 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   string.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 13:18:53 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/13 13:18:57 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/13 20:04:49 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int		ft_get_num(char *str);
 
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
+	while (str[i])
 		i++;
-	}
 	return (i);
 }
 
-char	*ft_strstr(char *str, char *to_find)
+char	*ft_get_file(int fildes)
 {
-	int	counter;
-	int	index;
+	int		total;
+	char	tempbuf[64];
+	int		readsz;
+	char	*out;
 
-	counter = 0;
-	if (to_find[0] == '\0')
-		return (str);
-	while (str[counter] != '\0')
+	total = 0;
+	readsz = -1;
+	while (readsz != 0)
 	{
-		index = 0;
-		while (str[counter + index] == to_find[index])
-		{
-			if (to_find[index + 1] == '\0')
-				return (str + counter);
-			index++;
-		}
-		counter++;
+		readsz = read(fildes, tempbuf, 64);
+		total += readsz;
 	}
-	return (0);
+	out = malloc(total + 1);
+	close(fildes);
+	open("numbers.dict", O_RDONLY);
+	read(fildes, out, total + 1);
+	out[total + 2] = 0;
+	return (out);
+}
+
+char	*ft_get_name_num(char *dict, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (dict[i])
+	{
+		if (i == 0 || dict[i - 1] == '\n')
+		{
+			if (ft_get_num(&dict[i]) == nb)
+			{
+				while (dict[i] && dict[i] != ':')
+				{
+					i++;
+				}
+				return (&dict[i + 2]);
+			}
+		}
+		i++;
+	}
+	return (NULL);
 }
